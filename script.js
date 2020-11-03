3aa15bc8723b6e186f673b9891930ecde13f2d0f
Katelynn Cowan <kcowan@twu.edu>
2020-11-03 10:03:26 -0600

    Created two variables and three functions for the quiz

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+var startButton = document.getElementById('start-btn')
+var questionContainerElement = document.getElementById('question-container')
+startButton.addEventListener('click', startQuiz)
 
+
+function startQuiz() {
+console.log('Started');
+startButton.classList.add('hide')
+questionContainerElement.classList.remove('hide')
+}
+
+function nextQuestion () {
+
+}
+
+function selectAnswer () {
+
+}
