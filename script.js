25b10f82608034f6b08356f861a546d45c459a2b
Katelynn Cowan <kcowan@twu.edu>
2020-11-03 10:18:59 -0600

    added questions and answers, including booleans to represent which answers are correct

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function startQuiz() {
 console.log('Started');
 startButton.classList.add('hide')
 questionContainerElement.classList.remove('hide')
+nextQuestion()
 }
 
 function nextQuestion () {
@@ -16,3 +17,51 @@ function nextQuestion () {
 function selectAnswer () {
 
 }
+
+var questions = [
+    {
+        question: 'Common data types do NOT include:',
+        answers: [
+            {text: 'numbers', correct: false},
+            {text: 'booleans', correct: false},
+            {text: 'strings', correct: false},
+            {text: 'curly brackets', correct: true}
+        ]
+    }
+    {
+        question: 'String values must be enclosed within:',
+        answers: [
+            {text: '{ }', correct: false},
+            {text: '( )', correct: false},
+            {text: ' " " ', correct: true},
+            {text: ' : : ', correct: false}
+        ]
+    }
+    {
+        question: 'What are booleans?',
+        answers: [
+            {text: 'variables', correct: false},
+            {text: 'true/false data type', correct: true},
+            {text: 'CSS styling property', correct: false},
+            {text: 'Indicates an ID', correct: false}
+        ]
+    }
+    {
+        question: 'What does || mean in JavaScript?',
+        answers: [
+            {text: 'equal to', correct: false},
+            {text: 'or', correct: true},
+            {text: 'and', correct: false},
+            {text: 'if else', correct: false}
+        ]
+    }
+    {
+        question: 'How do you communicate with the console?',
+        answers: [
+            {text: 'console.log(" ")', correct: true},
+            {text: '{Hello Console!}', correct: false},
+            {text: 'var communicateWith = console;', correct: false},
+            {text: 'What\'s a console?', correct: false}
+        ]
+    }
+]
